3c1ff2ed62bc5be563bf7b752e417f877ec0f99f
sangamithramailsamy <sangamithramailsamy@gmail.com>
2025-07-28 12:00:26 +0530

    modfied index.html file

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME', 'sangamithramailsamy@gmail.com')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD', 'YOUR_NEW_APP_PASSWORD_HERE')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD', 'ovay zueb uomo qrfo')
 app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_DEFAULT_SENDER', 'sangamithramailsamy@gmail.com')
 
 mail = Mail(app)
